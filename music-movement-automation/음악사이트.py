558261aac64eadd9aadf4c57de7a75878bf2c044
MinChan <76563710+kmc7582s@users.noreply.github.com>
2022-02-08 23:46:55 +0900

    Add files via upload

--- music-movement-automation/음악사이트.py
+++ music-movement-automation/음악사이트.py
@@ -15,23 +15,33 @@ id = browser.find_element_by_css_selector("#email")
 id.click()
 pyperclip.copy("아이디")
 pyautogui.hotkey("ctrl","v")
-time.sleep(1)
+time.sleep(2)
 
 #PW
 pw = browser.find_element_by_css_selector("#password")
 pw.click()
 pyperclip.copy("비밀번호")
 pyautogui.hotkey("ctrl","v")
-time.sleep(1)
+time.sleep(2)
 
 #login button
 login_btn = browser.find_element_by_css_selector("#btnSubmitSignin")
 login_btn.click()
-time.sleep(1)
+time.sleep(2)
 
 #플레이리스트로 이동
 browser.get("https://www.music-flo.com/storage/mylist")
 time.sleep(1)
 
+browser.get("https://www.music-flo.com/detail/mylist/azenzzniaydalony")
+time.sleep(1)
+
 #플레이리스트 복사
+pyautogui.moveTo(595,58, 2)
+pyautogui.drag(638,53, 2)
+pyautogui.hotkey("ctrl","c")
+
+#옮길 음악사이트로 이동
+browser.get("https://www.melon.com/")
+
 
